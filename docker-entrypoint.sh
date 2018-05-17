386296bea6cf6ac9d667c2e977fae48610fd7315
buzzi <buzzi.javier@gmail.com>
2018-05-17 23:23:29 +0200

    WIP

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,8 @@ SLAPD_CONF="/etc/openldap/slapd.ldif"
 SLAPD_DB="/var/lib/openldap/config"
 SLAPD_CERTS="/etc/openldap/certs"
 
+mkdir -p /var/lib/openldap/mdb
+
 if [ ! -d $SLAPD_DB ]; then
   mkdir -p $SLAPD_DB
   slapadd -n 0 -F $SLAPD_DB -l $SLAPD_CONF -d 3
